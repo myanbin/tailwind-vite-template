8901aaa72e425e328f26974e4b984e97e599fd87
马艳彬 <mayanbin@xinhua.org>
2022-06-22 15:16:57 +0800

    init: initial with vite + tailwind

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import HeroComponent from './components/HeroComponent.vue'
+</script>
+
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <HeroComponent message="Hello Vue 3 + TypeScript + Vite" />
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HeroComponent.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+
+import { ref } from 'vue'
+
+defineProps<{
+  message: string
+}>()
+
+const count = ref(0)
+</script>
+
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+    <button type="button" @click="count++">count is: {{ count }}</button>
+  </div>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
